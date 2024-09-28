4340bec904407a247e0491c66fb3bce216214eac
Artem Peshkov <pewkoff@gmail.com>
2024-09-28 21:06:32 +0300

    add file person and add struc Person

---
+++ NewWorkProjekt/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  NewWorkProjekt
+//
+//  Created by Lepestok Jora on 28.09.2024.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surName: String
+    let age: Int
+    let height: Double
+}
