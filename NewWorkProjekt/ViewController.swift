3ac588783ef72f26cc18cffd610fa4c115fa2ab2
Artem Peshkov <pewkoff@gmail.com>
2024-09-28 19:06:09 +0300

    append print

---
+++ NewWorkProjekt/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NewWorkProjekt
+//
+//  Created by Lepestok Jora on 28.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       printContent("Hello word")
+    }
+}
+
