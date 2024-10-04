0dd793e8ec6511ac7f49cf02c5096d41754bbc65
Artem Peshkov <pewkoff@gmail.com>
2024-10-05 00:42:34 +0300

    add shadow UIImage

--- NewWorkProjekt/ViewController.swift
+++ NewWorkProjekt/ViewController.swift
@@ -12,30 +12,35 @@ class ViewController: UIViewController {
     private let helper = Helper()
     private let textLable = UILabel()
     private let imageView = UIImageView()
+    private let imageContainerView = UIView()
+    private let imageCV = UIImageView()
     //private lazy let textLable: UILabel = setupLableTwo() (третий способ)
     
     //Второй способ (замыкание)
     /* private let setuoLable: UILabel = {
-        let lable = UILabel()
-        lable.text = "Hi"
-        lable.font = .systemFont(ofSize: 30, weight: .bold)
-        lable.textColor = .red
-        return lable
-    }()
-    */
+     let lable = UILabel()
+     lable.text = "Hi"
+     lable.font = .systemFont(ofSize: 30, weight: .bold)
+     lable.textColor = .red
+     return lable
+     }()
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .green
         updateNumbers()
-        
         setupLable()
-        setuoImageView()
-        setupView()
+        setupImageView()
+        
         view.addSubview(textLable)
         view.addSubview(imageView)
+        view.addSubview(imageContainerView)
+        view.addSubview(imageCV)
         
-        
+        setupView()
+        setupImageCV()
+        setupImageContainerView()
     }
     
     private func updateNumbers() {
@@ -50,28 +55,31 @@ class ViewController: UIViewController {
         let firsNumber = helper.getNumbers().first
         textLable.text = "\(firsNumber ?? 0)"
         textLable.font = .systemFont(ofSize: 30, weight: .bold)
+        textLable.backgroundColor = .yellow
         textLable.textColor = .red
+        textLable.layer.cornerRadius = 10
         textLable.frame = .init(x: 30, y: 30, width: 100, height: 50)
+        textLable.clipsToBounds = true
     }
     // Третий способ (вызываем прямо из экземпляра класса)
     /*private func setupLableTwo() -> UILabel {
-        let lable = UILabel()
-        lable.text = "Hi"
-        lable.font = .systemFont(ofSize: 30, weight: .bold)
-        lable.textColor = .red
-        return lable
-    }
+     let lable = UILabel()
+     lable.text = "Hi"
+     lable.font = .systemFont(ofSize: 30, weight: .bold)
+     lable.textColor = .red
+     return lable
+     }
      */
     
-    private func setuoImageView() {
-        imageView.image = UIImage(named: "Dog")
+    private func setupImageView() {
+        imageView.image = UIImage(named: "cat")
         imageView.frame = .init(x: 30, y: 130, width: 100, height: 200)
-        imageView.layer.cornerRadius = 30
+        imageView.layer.cornerRadius = 10
         imageView.clipsToBounds = true
-//        imageView.layer.shadowColor = UIColor.black.cgColor
-//        imageView.layer.shadowOffset = CGSize(width: 15, height: 15)
-//        imageView.layer.opacity = 1
-//        imageView.layer.shadowRadius = 10
+        imageView.layer.shadowColor = UIColor.black.cgColor
+        imageView.layer.shadowOffset = CGSize(width: 15, height: 15)
+        imageView.layer.opacity = 1
+        imageView.layer.shadowRadius = 10
     }
     
     private func setupView() {
@@ -84,6 +92,25 @@ class ViewController: UIViewController {
         //Добовляем подсклой к кнопке
         view.layer.insertSublayer(gradien, at: 0)
         
+    }
+    
+    private func setupImageContainerView() {
+        imageContainerView.frame = CGRect(x: 30, y: 130, width: 100, height: 200)
+        imageContainerView.backgroundColor = .brown
+        imageContainerView.layer.shadowColor = UIColor.black.cgColor
+        imageContainerView.layer.shadowOffset = CGSize(width: 20, height: 20)
+        imageContainerView.layer.shadowOpacity = 0.7
+        imageContainerView.layer.cornerRadius = 10
+        //imageContainerView.clipsToBounds = true
+        
+    }
+    
+    private func setupImageCV() {
+        imageCV.frame = CGRect(x: 30, y: 130, width: 100, height: 200)
+        imageCV.image = UIImage(named: "dog")
+        imageCV.tintColor = .black
+        imageCV.layer.cornerRadius = 10
+        imageCV.clipsToBounds = true
     }
 }
 
